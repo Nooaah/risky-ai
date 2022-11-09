30fbaa61a6ec177599786297a73930cb63314a92
Nooaah <noah.chtl@gmail.com>
2022-11-09 13:14:15 +0100

    Add ourPlayer v2 Monster

--- teamBlue/ourPlayer.py
+++ teamBlue/ourPlayer.py
@@ -33,13 +33,51 @@ class Player(hg.AbsPlayer) :
                     adjacent = self.game.edgesFrom(i)
                     if self.game.armyOn(i) != False:
                         if self.game.armyOn(i).status() == self.playerId:
+                            if i == 9 and self.game.armyOn(i).attributes()[1] >= 24:
+                                for j in adjacent:
+                                    nb_cases_vides = 0
+                                    if self.game.armyOn(j) == False:
+                                        if nb_cases_vides == 0:
+                                            self.actions.append(['move', i, j, 23])
+                                            nb_cases_vides += 1
                             nb_army = self.game.armyOn(i).attributes()[1] // 4
+                            start_point = 0
+                            ally_army = 0
+                            ennemy_army = 0
+                            ennemies = []
+                            nb_empty = 0
                             for j in adjacent:
                                 if j != 9:
                                     army = self.game.armyOn(j)
                                     if army == False:
+                                        nb_empty += 1
                                         if nb_army != 0:
                                             self.actions.append(['move', i, j, nb_army])
+                                    elif army.status() == self.playerId:
+                                        if i == 1 or i == 2:
+                                            start_point += 1
+                                        else:
+                                            ally_army += 1
+                                    elif army.status() != self.playerId:
+                                        ennemy_army += 1
+                                        ennemies.append(j)
+                                else:
+                                    if start_point == 3 and self.game.armyOn(i).attributes()[1] >= 24:
+                                        self.actions.append(['move', i, j, 23])
+                            if self.game.armyOn(i).attributes()[1] >= 24 and nb_empty == 0:
+                                if ennemy_army == 1:
+                                    if self.game.armyOn(ennemies[0]).attributes()[1] < 18:
+                                        self.actions.append(['move', i, ennemies[0], 23])
+                                elif ennemy_army > 1:
+                                    nb_ennemy_army = []
+                                    for j in ennemies:
+                                        nb_ennemy_army.append(self.game.armyOn(j).attributes()[1])
+                                    nb_less_ennemy_army = min(nb_ennemy_army)
+                                    for j in ennemies:
+                                        if self.game.armyOn(j).attributes()[1] == nb_less_ennemy_army:
+                                            self.actions.append(['move', i, j, 23])
+
+
                 for i in idcell:
                     if self.game.armyOn(i) != False:
                         if self.game.armyOn(i).status() == self.playerId:
